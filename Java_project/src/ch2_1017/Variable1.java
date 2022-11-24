85c633859c04b3f2e4e434eb9f73e63c2c3d372c
hahyeon16 <fjdkfh@naver.com>
2022-11-24 16:38:24 +0900

    1124 저장

--- Java_project/src/ch2_1017/Variable1.java
+++ Java_project/src/ch2_1017/Variable1.java
@@ -4,10 +4,24 @@ public class Variable1 {
 
 	public static void main(String[] args) {
 
-		int level;              //정수형 변수 level을 선언
+		int level;       //정수형 변수 level을 선언
 		level = 10;		//level 변수에 10을 대입
-		byte b1 = 12;
-		System.out.println(level); 
+		// 초기화: 최초의 변수에 값을 할당 하는 것 
+		byte b1 = 12; // 변수를 선언과 동시에 값을 할당.
+		// 기본 데이터 타입; PRIMITIVE TYPE.
+		// 기본 타입을 제외한 나머지는 참조형 타입 (주로 참조형을 많이 사용)
+		//예) 참조형 타입: 클래스 타입.
+		//Class Person
+		// 참조형 타입으로 선언하게 되면
+		//person p; 선언만
+		//p1 = new person(); 기본 생성자 호출
+		//생성자의 역할은 값을 초기화 하는 역할.
+		System.out.println(level);
+		System.out.println("내용 테스트 중");
+		System.out.println("단축키 설정이 잘 되었음");
+		System.out.println("간혹 단축키 실행이 안된다면 설정에서 추가해주기.");
+		//ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz. 입력
+		
 	}
 
 }
